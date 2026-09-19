f1ef04dcb547e9df66e32020742fadea3674e483
Danish <ansaridanish9648@gmail.com>
2026-09-19 20:15:07 +0530

    fix cors for new vercel url

--- server/server.js
+++ server/server.js
@@ -43,6 +43,7 @@ app.use(
             "http://localhost:5173",
             "http://localhost:5174",
             "https://skillbridge-sand-three.vercel.app",
+            "https://skillbridge-expark.vercel.app",
         ],
 
         credentials: true,
